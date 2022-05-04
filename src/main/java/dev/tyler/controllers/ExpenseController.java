c058b6a7bb39886cf29191fda59b3368fb693cfa
Zanetta Tyler <zrtyler@aggies.ncat.edu>
2022-05-04 10:06:05 -0400

    Update with status codes

--- src/main/java/dev/tyler/controllers/ExpenseController.java
+++ src/main/java/dev/tyler/controllers/ExpenseController.java
@@ -8,10 +8,11 @@ import dev.tyler.services.ExpenseService;
 import dev.tyler.services.ExpenseServiceImpl;
 import io.javalin.http.Context;
 
+import java.util.Locale;
+
 public class ExpenseController {
     public static ExpenseService expenseService = new ExpenseServiceImpl(new ExpenseDAOPostgresImpl());
 
-    public static void placholder(Context context){}
     // Get all employees
     public static void getExpenses(Context context){
         String status = context.queryParam("status");
@@ -30,31 +31,68 @@ public class ExpenseController {
     public static void viewExpense(Context context){
         int id = Integer.parseInt(context.pathParam("id"));
         Gson gson = new Gson();
-        String json = gson.toJson(expenseService.getExpense(id));
-        context.result(json);
+        Expense e = expenseService.getExpense(id);
+        if(e != null){
+            String json = gson.toJson(e);
+            context.result(json);
+        }else{
+            context.result("Expense not found.");
+            context.status(404);
+        }
     }
 
     public static void deleteExpense(Context context){
         int id = Integer.parseInt(context.pathParam("id"));
-        Gson gson = new Gson();
-        String json = gson.toJson(expenseService.removeExpense(id));
-        context.result(json);
+        Expense e = expenseService.getExpense(id);
+        if(e != null){
+            if(e.getStatus().equalsIgnoreCase("PENDING")){
+                boolean result = expenseService.removeExpense(id);
+                if(result){
+                    context.result("Expense was deleted.");
+                }else{
+                    context.result("Expense was not deleted.");
+                }
+            }else{
+                context.result("This expense cannot be deleted. It has already been " + e.getStatus().toLowerCase() + ".");
+            }
+        }else{
+            context.result("Expense not found.");
+            context.status(404);
+        }
     }
 
     public static void approveExpense(Context context){
         int id = Integer.parseInt(context.pathParam("id"));
         Gson gson = new Gson();
         Expense expense = expenseService.getExpense(id);
-        String json = gson.toJson(expenseService.approveExpense(expense));
-        context.result(json);
+        if(expense != null){
+            if(expense.getStatus().equalsIgnoreCase("PENDING")){
+                String json = gson.toJson(expenseService.approveExpense(expense));
+                context.result(json);
+            }else{
+                context.result("This expense has already been " + expense.getStatus().toLowerCase() + ".");
+            }
+        }else{
+            context.result("Expense not found.");
+            context.status(404);
+        }
     }
 
     public static void denyExpense(Context context){
         int id = Integer.parseInt(context.pathParam("id"));
         Gson gson = new Gson();
         Expense expense = expenseService.getExpense(id);
-        String json = gson.toJson(expenseService.denyExpense(expense));
-        context.result(json);
+        if(expense != null){
+            if(expense.getStatus().equalsIgnoreCase("PENDING")){
+                String json = gson.toJson(expenseService.approveExpense(expense));
+                context.result(json);
+            }else{
+                context.result("This expense has already been " + expense.getStatus().toLowerCase() + ".");
+            }
+        }else{
+            context.result("Expense not found.");
+            context.status(404);
+        }
     }
 
     public static void updateExpense(Context context){
@@ -63,15 +101,31 @@ public class ExpenseController {
         Gson gson = new Gson();
         Expense expense = gson.fromJson(body, Expense.class);
         expense.setId(id);
-        expenseService.updateExpense(expense);
-        context.result("Expense was updated.");
+
+        if(expense != null){
+            if(expense.getStatus().equalsIgnoreCase("PENDING")){
+                expenseService.updateExpense(expense);
+                context.result("Expense was updated.");
+            }else{
+                context.result("This expense cannot be updated. It has already been " + expense.getStatus().toLowerCase() + ".");
+            }
+        }else{
+            context.result("Expense not found.");
+            context.status(404);
+        }
     }
 
     public static void addExpense(Context context){
         String body = context.body();
         Gson gson = new Gson();
         Expense expense = gson.fromJson(body, Expense.class);
-        expenseService.addExpense(expense);
-        context.result("A new employee was created.");
+
+        if(expense != null){
+            expenseService.addExpense(expense);
+            context.result("A new expense was created.");
+            context.status(201);
+        }else{
+            context.result("A new expense was not created at this time.");
+        }
     }
 }
